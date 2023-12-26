9d451572637e8ea695166499ce28f2a0b922c370
bbaiserkeev <bbaiserkeev@s-pro.io>
2023-12-26 21:27:16 +0600

    service_app/drf_api_endpoint

--- service/service/settings.py
+++ service/service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clients',
+    'services'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
